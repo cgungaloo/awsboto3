484259c6eb09b1d992631a8ab3a7f5f904f74a64
cgungaloo <cgungaloo@yahoo.com>
2024-01-08 14:01:05 +0000

    cf with lambda function

---
+++ cloudformation/general_tutorials/lambda/lambda_function.py
@@ -0,0 +1,33 @@
+import json
+
+def lambda_handler(event, context):
+    # parse query strings
+    x = event['queryStringParameters']['x']
+    y = event['queryStringParameters']['y']
+    op = event['queryStringParameters']['op']
+    
+    print(f"x:{x}, y:{y}, op:{op}")
+    res_body ={}
+    res_body['x'] = int(x)
+    res_body['y'] = int(y)
+    res_body['op'] = op
+    if op == 'multi':
+        res_body['ans'] = multi(res_body['x'],res_body['y'])
+    else:
+        res_body['ans'] = add(res_body['x'],res_body['y'])
+    print(res_body)
+    
+    http_res = {}
+    http_res['statusCode'] = 200
+    http_res['headers'] = {}
+    http_res['headers']['Content-Type'] = 'application/json'
+    http_res['body'] = json.dumps(res_body)
+    
+    return http_res
+    
+    
+def multi(x,y):
+    return x * y
+
+def add(x,y):
+    return x + y

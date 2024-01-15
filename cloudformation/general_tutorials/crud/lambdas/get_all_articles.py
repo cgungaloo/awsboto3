6c393eb9358c3323cb8b15fabfe9e824f6485ac9
cgungaloo <cgungaloo@yahoo.com>
2024-01-15 12:33:01 +0000

    Client exception

--- cloudformation/general_tutorials/crud/lambdas/get_all_articles.py
+++ cloudformation/general_tutorials/crud/lambdas/get_all_articles.py
@@ -44,6 +44,7 @@ def lambda_handler(event, context):
         logger.info(f'Returning 500 error')
         http_res['statusCode'] = 500
         http_res['body'] = json.dumps(f'Got ClientError: {str(ce)}')
+        return http_res
     
         
 

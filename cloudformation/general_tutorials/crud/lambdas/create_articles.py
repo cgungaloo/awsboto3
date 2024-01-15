a9b7bc2acacd587240fe34c702cfc07d5317e02c
cgungaloo <cgungaloo@yahoo.com>
2024-01-15 14:58:13 +0000

    Added client error test and called with

--- cloudformation/general_tutorials/crud/lambdas/create_articles.py
+++ cloudformation/general_tutorials/crud/lambdas/create_articles.py
@@ -39,6 +39,13 @@ def lambda_handler(event, context):
         http_res['statusCode'] = 500
         http_res['body'] = json.dumps(f'Got ClientError: {str(ce)}')
         return http_res
+    except KeyError as ke:
+        logger.info(f'Exception : {str(ke)}')
+        logger.info(f'Returning 400 error')
+        http_res['statusCode'] = 400
+        http_res['body'] = json.dumps(f'Got Key Error: {str(ke)}')
+        return http_res
+    
 
 
     http_res['statusCode'] = response['ResponseMetadata']['HTTPStatusCode']
--- cloudformation/general_tutorials/crud/lambdas/tests/test_crud_articles.py
+++ cloudformation/general_tutorials/crud/lambdas/tests/test_crud_articles.py
@@ -1,7 +1,13 @@
 from unittest import TestCase
+from unittest import mock
 from awsboto3.cloudformation.general_tutorials.crud.lambdas.get_all_articles import lambda_handler as get_all_lambda
+from awsboto3.cloudformation.general_tutorials.crud.lambdas.create_articles import lambda_handler as create_lambda
 from unittest.mock import patch, Mock
 from boto3.dynamodb.conditions import Attr
+from botocore.stub import Stubber
+import boto3
+
+from awsboto3.cloudformation.general_tutorials.crud.lambdas.tests.test_helpers.lambda_context import LambdaContext
 
 class Test(TestCase):
     
@@ -36,10 +42,62 @@ class Test(TestCase):
         assert response['statusCode'] == 400
         assert response['headers']['Content-Type'] == 'application/json'
         assert response['body'] == '"Got Key Error: \'title\'"'
+    
+    @patch("boto3.resource")
+    def test_create_key_error(self, mock_resource):
+        event = {"queryStringParameters": 
+                    {"badkey":"mytitle"}}
+        context = "context_test"
+
+        response = get_all_lambda(event, context)
+
+        assert response['statusCode'] == 400
+        assert response['headers']['Content-Type'] == 'application/json'
+        assert response['body'] == '"Got Key Error: \'title\'"'
+
+    @patch("boto3.resource")
+    def test_create_article(self, mock_resource):
+        event = {"queryStringParameters": 
+                    {"title":"mytitle",
+                     "description":"mydescription"}}
+        context = LambdaContext()
+
+        mock_table = Mock()
+        mock_table.put_item.return_value = {'ResponseMetadata':
+                                            {'HTTPStatusCode':200}
+                                            }
+        mock_resource.return_value.Table.return_value = mock_table
+
+        response = create_lambda(event, context)
+
+        mock_table.put_item.assert_called_with(
+            Item ={
+                'id':context.aws_request_id,
+                'title': event['queryStringParameters']['title'],
+                'description': event['queryStringParameters']['description'],
+                'published': False,
+                'createdAt': mock.ANY,
+                'updatedAt': mock.ANY
+            }
+        )
+
         
+        assert response['statusCode'] == 200
+        assert response['body'] == 'Record abc123 added'
 
+    def test_create_client_error(self):
+        event = {"queryStringParameters": 
+                    {"title":"mytitle",
+                     "description":"mydescription"}}
+        context = LambdaContext()
 
+        db_resource = boto3.resource('dynamodb')
+        resource_stubber = Stubber(db_resource.meta.client)
 
+        resource_stubber.add_client_error('put_item','LimitExceededException')
+        resource_stubber.activate()
 
-
-        
+        with mock.patch('boto3.resource', mock.MagicMock(return_value=db_resource)):
+            response = create_lambda(event, context)
+            assert response['body'] == '"Got ClientError: An error occurred (LimitExceededException) when calling the PutItem operation: "'
+            assert response['statusCode'] == 500
